2af9beca68d9590a2adc0256b016d8545c01d9b0
Eric L. Hepperle <codewizard13@gmail.com>
2025-03-16 14:24:38 -0500

    UPDATE front-page.php:  MIGRATE SVG defs to template-parts/svg.php

--- front-page.php
+++ front-page.php
@@ -17,7 +17,8 @@
 
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
 
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 	<style>
 		.bd-placeholder-img {
@@ -106,7 +107,7 @@
 </head>
 
 <body>
-<?php echo get_template_part('template-parts/svg');?>
+	<?php echo get_template_part('template-parts/svg'); ?>
 
 	<!-- Dropdown menu for dark/light -->
 	<div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
@@ -155,24 +156,7 @@
 				</button>
 			</li>
 		</ul>
-	</div>
-
-
-	<svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-		<symbol id="aperture" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-			stroke-width="2" viewBox="0 0 24 24">
-			<circle cx="12" cy="12" r="10" />
-			<path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />
-		</symbol>
-		<symbol id="cart" viewBox="0 0 16 16">
-			<path
-				d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-		</symbol>
-		<symbol id="chevron-right" viewBox="0 0 16 16">
-			<path fill-rule="evenodd"
-				d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
-		</symbol>
-	</svg>
+	</div><!-- END dropdown -->
 
 	<div class="container">
 		<header class="border-bottom lh-1 py-3">
@@ -528,7 +512,9 @@
 			<a href="#">Back to top</a>
 		</p>
 	</footer>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+		crossorigin="anonymous"></script>
 
 
 </body>
